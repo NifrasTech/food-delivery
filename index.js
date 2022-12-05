0cf66f5b2ba1d02caffdb267944d412fe8c0b779
Nifras <nifrasusanar@outlook.com>
2022-12-05 17:45:17 +0300

    Routes updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const database = require('./config/database')
+const routes = require('./config/routes')
 const path = require("path")
 require('dotenv').config()
 
@@ -9,9 +10,8 @@ const port = process.env.PORT || 4000
 //Static file
 app.use(express.static(path.join(__dirname, "public")))
 
+app.use('/',routes)
 
-
-app.listen(port,async ()=>{
-    await database.getDatabase()
+app.listen(port,()=>{
     console.log('Listening to the port:'+port)
 })
