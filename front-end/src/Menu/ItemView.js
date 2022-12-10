d6ed661929bbc48f7619941f230e73b01405d1bf
Nifras <nifrasusanar@outlook.com>
2022-12-10 13:33:52 +0300

    Item view designed

--- front-end/src/Menu/ItemView.js
+++ front-end/src/Menu/ItemView.js
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { Box } from "@mui/material";
+import { Box, Paper, TextField, IconButton, Badge } from "@mui/material";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import AppBar from "@mui/material/AppBar";
 import CardMedia from "@mui/material/CardMedia";
 import Stack from "@mui/material/Stack";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
+import LocalMallIcon from "@mui/icons-material/LocalMall";
 import Typography from "@mui/material/Typography";
 import BackIcon from "@mui/icons-material/ArrowBack";
 import Slide from "@mui/material/Slide";
@@ -37,9 +37,11 @@ export default function ItemView({ handleClose, open }) {
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
               Food Information
             </Typography>
-            <Button color="inherit" onClick={handleClose}>
-              save
-            </Button>
+            <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="error">
+                <LocalMallIcon />
+              </Badge>
+            </IconButton>
           </Toolbar>
         </AppBar>
         <Stack spacing={2}>
@@ -75,7 +77,31 @@ export default function ItemView({ handleClose, open }) {
               eiusmod id ut laborum elit ex amet velit veniam.
             </Typography>
           </Box>
+          
         </Stack>
+        <Paper 
+        sx={{
+          height: '70px', marginTop: 'auto', 
+          borderStartEndRadius:'30px', borderStartStartRadius:'30px',
+          display:'flex', alignItems:'center',
+          padding:'10px', paddingRight:'20px', paddingLeft:'20px'
+          }}
+        elevation={3}>
+          <TextField
+          id="outlined-number"
+          label="Quantity"
+          type="number"
+          inputProps={{ min: 1}}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          sx={{maxWidth:'130px', mr:'15px'}}
+        />
+          <Button variant="contained" size="large" sx={{width:'100%'}} onClick={handleClose}>
+            ADD TO CART
+          </Button>
+        </Paper>
+        
       </Dialog>
     </div>
   );
